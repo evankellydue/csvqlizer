5e30dc3ebe7e520dc2201fd9aea6ed1af12b2d09
Evan Kelly <evanclaykelly@gmail.com>
2025-08-30 11:01:49 -0500

    MVP of tool to get varchar sql table from CSV table

---
+++ README.md
@@ -0,0 +1,38 @@
+# CSVQLIZER
+
+This is a very rough project that aims to accomplish a common work task:
+
+Given a CSV export from a random records management system, how can I rename the columns to be dbeaver sql-autocomplete compatible and create a table to import to where the data type is varchar?
+
+#### Why varchar?
+
+I find editing/transforming/analyzing CSVs to be easier in postgres then in excel, because I can write whatever logic I want. I also prefer having empty values = null (as opposed to making everything text)
+It is a lot easier for me to treat everything as a string and do operations like `split_part`, `concat_ws`, and `ilike '%text%'` and transform the data into a new CSV for loading into an existing script I have with appropriate data types.
+
+#### What do you mean by dbeaver sql-autocomplete compatible?
+
+dbeaver's autocomplete is very good if you have created a table with columns like:
+
+`create table deployment.example`
+`(`
+`   column_one varchar,`
+`   column_two varchar`
+`)`
+
+and doesn't guess well if create a table like this:
+
+`create table deployment.bad_example`
+`(`
+`   "column one" varchar,`
+`   "column two" varchar`
+`)`
+
+#### What does this currently do?
+ - Identify the first line of a CSV with unix linebreaks (windows forthcoming because that's what I work on)
+ - Prints out a fixed first line of the CSV in the terminal to replace the first line in the input CSV
+ - Prints out columns in the example format above to help with creating a sql table to import the data into
+
+#### Planned Features
+ - Detect windows or linux line endings and behave appropriately
+ - Produce a file ending with `_sqlized.csv` with the corrected first line
+ - Produce a basic sql file giving the entire skeleton of the varchar table to be created
---
+++ csvqlizer.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int csv_row_bytes(char* csv_buffer);
+
+int main(int argc, char* argv[])
+{
+    // Check if only one file argument was provided
+    if (argc != 2)
+    {
+        printf("Usage: ./csvparser FILENAME\n");
+        return 1;
+    }
+    FILE* csv = fopen(argv[1], "r");
+    // Check the fopen succeeded
+    if (csv == NULL)
+    {
+        printf("fopen failed\n");
+        return 2;
+    }
+    // Determine how many bytes are in the first line of the CSV
+    int bytes = 0;
+    char character;
+    while ((character = fgetc(csv)) != EOF)
+    {
+        // Add a byte each time that fgetc returns a non-EOF character
+        bytes++;
+    }
+    // Bring us back to the start of the file
+    rewind(csv);
+    // Allocate a buffer for the csv to be read into
+    char* csv_buffer = malloc(bytes);
+    // Check that csv_buffer returned a value
+    if (csv_buffer == NULL)
+    {
+        printf("malloc for the csv_buffer failed\n");
+        return 3;
+    }
+    // Read the entire file into the buffer 
+    fread(csv_buffer, 1,  bytes, csv);
+    // Determine how many bytes are in the first line of the CSV
+    int first_row_bytes = csv_row_bytes(csv_buffer);
+    // Create a new string for just the first CSV row
+    char* first_row = malloc(first_row_bytes);
+    if (first_row == NULL)
+    {
+        printf("malloc for the first_row failed\n");
+        return 3;
+    }
+    // Copy the first_row from the csv_buffer
+    for (int i = 0; i < first_row_bytes; i++)
+    {
+        first_row[i] = csv_buffer[i];
+    }
+    // Create a buffer for the fixed_first_row
+    char* fixed_first_row = malloc(first_row_bytes);
+    if (fixed_first_row == NULL)
+    {
+        printf("malloc for the fixed_first_row failed\n");
+        return 3;
+    }
+    // Fix the first row, skipping quotations and fixing disallowed characters
+    // Names in SQL must begin with a letter (a-z) or underscore (_). Subsequent characters in a name can be letters, digits (0-9), or underscores
+    // Initialize the bytes of the first row
+    int fixed_first_row_bytes = 0;
+    for (int i = 0; i < first_row_bytes; i++)
+    {
+        // If the character is a comma, copy it over
+        if (first_row[i] == ',')
+        {
+            fixed_first_row[fixed_first_row_bytes] = first_row[i];
+            fixed_first_row_bytes++;
+        }
+        // If the character is allowed in a table_name, add it to the fixed first_row 
+        else if ((first_row[i] == 95) || ((first_row[i] <= 90) && (first_row[i] >= 65)) || ((first_row[i] <= 122) && (first_row[i] >= 97)) || ((first_row[i] <= 57) && (first_row[i] >= 48)))
+        {
+            // Set that character to be the next in the fixed_first_row
+            fixed_first_row[fixed_first_row_bytes] = first_row[i];
+            fixed_first_row_bytes++;
+        }
+        // If the character is a quotes, skip it, we don't need it
+        else if ((first_row[i] == '"') || (first_row[i] == '\n'))
+        {
+           // Do nothing
+        }
+        // If the character is a disallowed character
+        else
+        { 
+            fixed_first_row[fixed_first_row_bytes] = '_';
+            fixed_first_row_bytes++;
+        }
+    }
+    // Print out the new deployment table varchar columns
+    for (int i = 0; i < fixed_first_row_bytes; i++)
+    {
+        if (fixed_first_row[i] == ',')
+        {
+            printf(" varchar,\n");
+        }
+        else
+        {
+            printf("%c", fixed_first_row[i]);
+        }
+    }
+    printf(" varchar\n");
+    // Create a new file with the new first CSV row
+    // Print the SQL deployment table with every column as a varchar
+    // Close the the file when you are done
+    fclose(csv);
+}
+
+int csv_row_bytes(char* csv_buffer)
+{
+    int csv_row_bytes = 0;
+    // Detect if we are using windows or linux line endings
+    bool is_linux = true;
+    // Loop through csv_buffer until we find the end of a CSV_row
+    char current = 'a';
+    bool quote = false;
+    if (is_linux)
+    {
+        while (current != '\n')
+        {
+            // Set the first byte the CSV equal to current
+            current = csv_buffer[csv_row_bytes];
+            // If we encouter a quotation mark, set the quote flag equal to true
+            if (current == '"')
+            {
+                // This will just flicker it to be the opposite of what it was
+                quote = !quote;
+            }
+            // if the current flag is a new line but we are within quotes, ensure current doesn't break the while loop
+            if ((current == '\n') && (quote))
+            {
+                current = 'a';
+            }
+            // Increment the amount of bytes in the row of the csv
+            csv_row_bytes++;
+        }
+    }
+    else if (!is_linux)
+    {
+        printf("Windows support not developed yet.\n");
+        return 4;
+    }
+    return csv_row_bytes;
+}
+
